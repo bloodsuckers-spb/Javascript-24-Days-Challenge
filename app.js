165fd10bbce08f5eb45d5e817d5b0059dd35cd78
Vitaly <bloodsuckers-spb@hotmail.com>
2021-12-14 20:37:16 +0300

    feat: add password output

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-document.querySelector('.fields').addEventListener('click', function(e){
+const password = [1];
+
+document.querySelector('form').addEventListener('click', function(e){
     const input = e.target.closest('input');
+    const button = e.target.closest('button');
     if (input) onInput(input);
+    
+    if (button) {
+        e.preventDefault();
+        console.log(password);
+    }
+
 });
 
     function onInput(el) { 
         el.addEventListener('input', function() {
             this.value = this.value.replace(/\D/, '');
+            password.push(this.value);
+            console.log(this.value);
             if (el.value.length && el.nextElementSibling) {
                 el.nextElementSibling.focus();
                 onInput(el.nextElementSibling);
