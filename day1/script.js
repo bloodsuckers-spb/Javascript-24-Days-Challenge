afa885f3ef5d91025073fa2ea2a4c348a5750ad2
Vitaly <bloodsuckers-spb@hotmail.com>
2021-12-23 02:00:54 +0300

    feat: add functions toSwitch and timerOnInput

--- day1/script.js
+++ day1/script.js
@@ -3,54 +3,66 @@ const button = timer.querySelector('.start, .stop');
 const settings = timer.querySelector('.settings');
 const minutes = timer.querySelector('.minutes input');
 const seconds = timer.querySelector('.seconds input');
+const ring = document.querySelector('.wrapper .ring');
 const btnCaptureStart = 'start';
 const btnCaptureEnd = 'end';
-const ring = document.querySelector('.wrapper .ring');
 let timerId;
+
+const toSwitch = (item, bool) => {
+  if (Array.isArray(item)) {
+    item.forEach(el => el.disabled = bool);
+  }
+  else {
+      item.disabled = bool;
+  } 
+}
+
+const timerOnInput = () => {
+  timer.addEventListener('input', (e) => {
+    const flag = (!(+minutes.value + +seconds.value))
+    toSwitch(button, flag)
+    if (e.target.value > 59) e.target.value = 59;
+  });
+}
 
 const timePrettier = (time) => {
   return time.length < 2 ? time.padStart(2, '0') : time;
 };
 
+const timeIncrement = () => {
+  if (!+seconds.value) {
+    seconds.value = 60;
+    minutes.value--;
+  }
+  seconds.value--;
+  minutes.value = timePrettier(minutes.value);
+  seconds.value = timePrettier(seconds.value);
+};
+
 const timerStop = () => {
   clearInterval(timerId);
-  settings.disabled = false;
+  toSwitch(settings, false)
   if (button.disabled) {
     ring.classList.add('ending');
   }
 };
 
 const timerStart = () => {
-  settings.disabled = true;
-  minutes.disabled = true;
-  seconds.disabled = true;
-  timerId = setInterval(function () {
+  toSwitch([settings, minutes, seconds], true);
+  timerId = setInterval(() => {
     if (!(+minutes.value + +seconds.value)) {
-      button.disabled = true;
+      toSwitch(button, true);
       btnCapture();
       return;
     }
-    if (seconds.value == 0) {
-      seconds.value = 60;
-      minutes.value--;
-    }
-    seconds.value--;
-
-    minutes.value = timePrettier(minutes.value);
-    seconds.value = timePrettier(seconds.value);
+    timeIncrement();
   }, 1000);
 };
 
 const clickOnGear = () => {
   ring.classList.remove('ending');
-  minutes.disabled = false;
-  seconds.disabled = false;
-  timer.addEventListener('input', (e) => {
-    if (e.target.value > 59) e.target.value = 59;
-    if (+minutes.value + +seconds.value) {
-      button.disabled = false;
-    } else button.disabled = true;
-  });
+  toSwitch([minutes, seconds], false)
+  timerOnInput();
 };
 
 const btnCapture = () => {
@@ -66,7 +78,6 @@ const btnCapture = () => {
 const timerClicked = (e) => {
   const target = e.target.closest('button');
   e.stopImmediatePropagation();
-
   if (target === button) btnCapture();
   if (target === settings && !settings.disabled) {
     clickOnGear();
