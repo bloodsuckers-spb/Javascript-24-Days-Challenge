f0c22392ae2aed9f3b3c48a2090686975f2b125b
Vitaly <bloodsuckers-spb@hotmail.com>
2021-12-29 00:29:13 +0300

    feat: change the structure of project, hang blur listener on form

--- day19/app.js
+++ day19/app.js
@@ -1,73 +1,76 @@
 const form = document.querySelector('form');
 const data = {};
 
-const showValidationCheck = (el, bool) => {
-  const errorField = el.parentElement.querySelector('.error');
-  const successField = el.parentElement.querySelector('.success');
-  
-  if (bool) {
-    successField.innerHTML = '<img src="./images/success.svg" alt="Success" />';
-    errorField.innerHTML = '';
-  } else {
-    successField.innerHTML = '';
-    errorField.innerHTML = `<img src="./images/error.svg" alt="Error" />
-      //   A ${el.name} is required`;
-  }
-};
-
-const toValidate = (el) => {
-  if (!el.value.length) {
-    showValidationCheck(el, false);
-    return;
-  }
-
-  // валидация EMAIL
-
-  if (el.name === 'confirm-password') {
-    if (data[el.name] !== data.password) {
-      showValidationCheck(el, false);
-      return;
-    }
-  }
-    showValidationCheck(el, true);
-};
-
-const addInputListeners = (el) => {
-  el.addEventListener('input', (e) => {
-    e.target.value = e.target.value.trim();
-    data[e.target.name] = e.target.value;
-  });
-  el.addEventListener('focus', (e) => {
-    console.log('focus');
-  });
-  el.addEventListener('blur', (e) => {
-    toValidate(e.target);
-  });
-};
-
 const switchPasswordVisibility = (target) => {
   const el = target.parentElement.querySelector('input');
   el.type = el.type === 'text' ? 'password' : 'text';
-} 
+};
+
+const showValidationCheck = (el, bool, str) => {
+  const [outputImg, outputText] =
+    el.parentElement.querySelector('.output').children;
+  outputText.innerHTML = '';
+
+  if (bool) {
+    outputImg.classList.remove('invalid');
+    outputImg.classList.add('valid');
+  } else {
+    outputImg.classList.remove('valid');
+    outputImg.classList.add('invalid');
+    if (str) {
+      outputText.innerHTML = `passwords fields should match`;
+      return;
+    }
+    outputText.innerHTML = `This field is required`;
+  }
+};
+
+const validate = (e) => {
+  let bool = false;
+
+  if (!e.target.value.length) {
+    showValidationCheck(e.target, false);
+    return;
+  }
+
+  if (e.target.name === 'name') {
+    bool = e.target.value.length >= 3;
+  }
+
+  if (e.target.name === 'email') {
+    bool = e.target.value.length >= 6;
+  }
+
+  if (e.target.name === 'password') {
+    bool = e.target.value.length >= 4;
+    if (bool) {
+      const confirm = form.querySelector('.confirm-password');
+      confirm.disabled = false;
+    }
+  }
+
+  if (e.target.name === 'confirm-password') {
+    bool = data[e.target.name] === data.password;
+    if (!bool) {
+      showValidationCheck(e.target, bool, e.target.name);
+      return;
+    }
+  }
+  showValidationCheck(e.target, bool);
+};
 
 const clickOnForm = (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    if (e.target.closest('.field__input')) {
-      addInputListeners(e.target)
-     
-    }
+  if (e.target.closest('.submit')) {
+    console.log('submit');
+  }
 
-    if (e.target.closest('.submit')) {
-      console.log('submit')
-    }
-
-
-    if (e.target.closest('.show-hide')) {
-      switchPasswordVisibility(e.target);
-    }
-
-}
+  if (e.target.closest('.show-hide')) {
+    switchPasswordVisibility(e.target);
+  }
+};
 
 
 form.addEventListener('click', clickOnForm);
+form.addEventListener('blur', validate, true);
