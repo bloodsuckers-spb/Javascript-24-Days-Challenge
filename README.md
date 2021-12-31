eb57c7ad2813732e0677e75b8ec496b331556ebe
Vitaly <bloodsuckers-spb@hotmail.com>
2021-12-31 20:23:30 +0300

    feat: implement functionality of day2 project!

--- README.md
+++ README.md
@@ -13,6 +13,22 @@
 
 Deploy: [Pomodoro Timer](https://bloodsuckers-spb.github.io/advent-of-js/day1/ "Pomodoro Timer")
 
+### Day 2: E-Commerce Component
+
+![E-Commerce Component](/images/day2-screen.png "E-Commerce Component")
+
+#### Users should be able to:
+
+*  View the plates on the left side of the screen and add them to your cart on the right side.
+* When there are no plates within your cart, you should see a message that says, "Your cart is empty."
+* When a plate is added to your cart, the Subtotal and Totals will automatically update.
+* When products are in your cart, you should be able to increase and decrease the quantity. 
+    - A user should not be able to mark the quantity as a negative number.
+    - If the quantity goes down to 0, the user will have the option to delete or remove the product from their cart entirely.
+* Tax is based on the state of Tennessee sales tax: 0.0975
+
+Deploy: [E-Commerce Component](https://bloodsuckers-spb.github.io/advent-of-js/day2/ "E-Commerce Component")
+
 
 ### Day 10: Password Verifier
 
---
+++ day2/app.js
@@ -0,0 +1,159 @@
+const menuItems = [
+  {
+    name: 'French Fries with Ketchup',
+    price: 2.23,
+    image: 'plate__french-fries.png',
+    alt: 'French Fries',
+    count: 2,
+  },
+  {
+    name: 'Salmon and Vegetables',
+    price: 5.12,
+    image: 'plate__salmon-vegetables.png',
+    alt: 'Salmon and Vegetables',
+    count: 0,
+  },
+  {
+    name: 'Spaghetti with Meat Sauce',
+    price: 7.82,
+    image: 'plate__spaghetti-meat-sauce.png',
+    alt: 'Spaghetti with Meat Sauce',
+    count: 0,
+  },
+  {
+    name: 'Bacon, Eggs, and Toast',
+    price: 5.99,
+    image: 'plate__bacon-eggs.png',
+    alt: 'Bacon, Eggs, and Toast',
+    count: 0,
+  },
+  {
+    name: 'Chicken Salad with Parmesan',
+    price: 6.98,
+    image: 'plate__chicken-salad.png',
+    alt: 'Chicken Salad with Parmesan',
+    count: 0,
+  },
+  {
+    name: 'Fish Sticks and Fries',
+    price: 6.34,
+    image: 'plate__fish-sticks-fries.png',
+    alt: 'Fish Sticks and Fries',
+    count: 1,
+  },
+];
+
+const menu = document.querySelector('.menu');
+const cart = document.querySelector('.cart');
+const totals = cart.querySelector('.totals');
+const cartSummary = cart.querySelector('.cart-summary');
+const subtotal = totals.querySelector('.subtotal');
+const tax = totals.querySelector('.tax');
+const total = totals.querySelector('.total .total');
+const quantityWrapperTemplate = cart.querySelector('.quantity__wrapper');
+
+const inCartString = `<img src="images/check.svg" alt="Check" />
+In Cart`;
+const addtoCartString = 'Add to Cart';
+
+const createElement = (type, className) => {
+  const el = document.createElement(type);
+  el.classList.add(className);
+  return el;
+};
+
+const emptyCartNotification = () => {
+  const p = createElement('p', 'empty');
+  p.textContent = `Your cart is empty.`;
+  cartSummary.prepend(p);
+};
+
+const fullSubTotalPrice = () => {
+  return menuItems
+    .filter((el) => el.count > 0)
+    .map((el) => el.price * el.count)
+    .reduce((el, acc) => {
+      return acc + el;
+    }, 0);
+};
+
+const renderTotalPrice = () => {
+  const subtotalValue = fullSubTotalPrice()
+  const taxValue = subtotalValue * 0.0975;
+  if (!subtotalValue) emptyCartNotification();
+  subtotal.textContent = `$` + subtotalValue.toFixed(2) ;
+  tax.textContent = `$` + taxValue.toFixed(2);
+  total.textContent = `$` + (taxValue + subtotalValue).toFixed(2);
+};
+
+const createItem = (el, ind) => {
+  const li = el.cloneNode(true);
+  const quantityWrapper = quantityWrapperTemplate.cloneNode(true);
+  const quantity = createElement('div', 'quantity');
+  const subtotal = createElement('div', 'subtotal');
+  const [plate, content] = li.children;
+  content.lastElementChild.remove();
+  quantity.textContent = ++menuItems[ind].count;
+  subtotal.textContent = `$${menuItems[ind].count * menuItems[ind].price}`;
+  li.append(quantityWrapper, subtotal);
+  plate.append(quantity);
+  return li;
+};
+
+const deleteItem = (el, ind) => {
+  const btn = menu.querySelector(`[data-ind = "${ind}"] .in-cart`);
+  el.remove();
+  btn.classList.remove('in-cart');
+  btn.classList.add('add');
+  btn.innerHTML = addtoCartString;
+};
+
+const renderItem = (el, ind) => {
+  const quantity = el.querySelectorAll('.quantity');
+  const subtotal = el.lastElementChild;
+  const subtotalPrice = menuItems[ind].count * menuItems[ind].price;
+  quantity.forEach((el) => {
+    el.textContent = menuItems[ind].count;
+  });
+  subtotal.textContent = `$${subtotalPrice.toFixed(2)}`;
+};
+
+const addToCard = (e) => {
+  const btn = e.target.closest('.add');
+
+  if (btn) {
+    const target = e.target.closest('li');
+    const index = target.dataset.ind;
+    const li = createItem(target, index);
+    btn.classList.remove('add');
+    btn.classList.add('in-cart');
+    btn.innerHTML = inCartString;
+    if (cart.querySelector('.empty')) {
+      cart.querySelector('.empty').replaceWith(li);
+    } else {
+      cartSummary.append(li);
+    }
+    renderTotalPrice();
+  }
+};
+
+const cartOnclick = (e) => {
+  const arrow = e.target.closest('button');
+
+  if (arrow) {
+    const target = e.target.closest('li');
+    const ind = target.dataset.ind;
+    if (arrow.classList[0] === 'increase') {
+      ++menuItems[ind].count;
+      renderItem(target, ind);
+    }
+    if (arrow.classList[0] === 'decrease') {
+      --menuItems[ind].count;
+      !menuItems[ind].count ? deleteItem(target, ind) : renderItem(target, ind);
+    }
+    renderTotalPrice();
+  }
+};
+
+menu.addEventListener('click', addToCard);
+cart.addEventListener('click', cartOnclick);
